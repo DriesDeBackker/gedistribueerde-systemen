1523943fa285adc079f702df6ba92095b6db9179
driesdebackker <dries.de.backker@gmail.com>
2017-10-17 12:37:34 +0200

    labsession JEE

--- assignment-JEE-1/CarRental-ejb/src/java/session/CarRentalSession.java
+++ assignment-JEE-1/CarRental-ejb/src/java/session/CarRentalSession.java
@@ -1,17 +1,55 @@
 package session;
 
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 import javax.ejb.Stateful;
+import rental.CarRentalCompany;
+
+import rental.Quote;
 import rental.RentalStore;
+import rental.ReservationConstraints
 
 @Stateful
 public class CarRentalSession implements CarRentalSessionRemote {
+
+    private Set<Quote> quotes;
 
     @Override
     public Set<String> getAllRentalCompanies() {
         return new HashSet<String>(RentalStore.getRentals().keySet());
     }
+    
+    @Override
+    public Quote createQuote(ReservationConstraints constraints){
+        Map<String, CarRentalCompany> rentals = RentalStore.getRentals();
+        Set<String> rentalNames = rentals.keySet();
+        int length = rentalNames.size();
+        boolean found = false;
+        int i = 0;
+        Iterator<String> iterator = rentalNames.iterator();
+        while(! found && iterator.hasNext()) {
+            CarRentalCompany rentalCompany = rentals.get(iterator.next());
+            if(rentalCompany.hasRegion(constraints.getRegion())
+                && rentalCompany.isAvailable(constraints.getCarType(), constraints.getStartDate(), constraints.getEndDate())) {
+                found = true;            
+        }
+        Quote newQuote =  new Quote(carRenter, constraints.getStartDate(), 
+                constraints.getEndDate(),constraints, rentalCompany,
+                constraints.getCarType(),constraints.getRegion());
+        this.quotes.add(newQuote);
+    };
+    
+    @Override
+    public Set<Quote> getCurrentQuotes(){
+        
+    };
+    
+    @Override
+    public void confirmQuotes(Set<Quote> quotes) throws ReservationException {
+        
+    };
 
     
     
--- assignment-JEE-1/CarRental-lib/src/session/CarRentalSessionRemote.java
+++ assignment-JEE-1/CarRental-lib/src/session/CarRentalSessionRemote.java
@@ -3,9 +3,19 @@ package session;
 import java.util.Set;
 import javax.ejb.Remote;
 
+import rental.Quote;
+import rental.ReservationConstraints;
+import rental.ReservationException;
+
 @Remote
 public interface CarRentalSessionRemote {
 
     Set<String> getAllRentalCompanies();
+    
+    Quote createQuote(ReservationConstraints constraints);
+    
+    Set<Quote> getCurrentQuotes();
+    
+    void confirmQuotes(Set<Quote> quotes) throws ReservationException;
     
 }
