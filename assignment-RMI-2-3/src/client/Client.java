4d706d55caf852d90b58f38f1d14ba8e32251286
driesdebackker <dries.debackker@student.kuleuven.be>
2017-10-27 20:56:55 +0200

    Nieuwe client + nieuwe interfaces (nog te implementeren)

--- assignment-RMI-2-3/src/client/Client.java
+++ assignment-RMI-2-3/src/client/Client.java
@@ -5,6 +5,7 @@ import java.rmi.registry.Registry;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 import rental.CarType;
 import rental.ICarRentalCompany;
@@ -12,7 +13,7 @@ import rental.Quote;
 import rental.Reservation;
 import rental.ReservationConstraints;
 
-public class Client extends AbstractTestBooking {
+public class Client extends AbstractTestManagement{
 	
 	/********
 	 * MAIN *
@@ -42,120 +43,77 @@ public class Client extends AbstractTestBooking {
 		this.crc = crc;
 	}
 	
-	/**
-	 * Check which car types are available in the given period
-	 * and print this list of car types.
-	 *
-	 * @param 	start
-	 * 			start time of the period
-	 * @param 	end
-	 * 			end time of the period
-	 * @throws 	Exception
-	 * 			if things go wrong, throw exception
-	 */
+
 	@Override
-	protected void checkForAvailableCarTypes(Date start, Date end) throws Exception {
+	protected Set getBestClients(Object ms) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String getCheapestCarType(Object session, Date start, Date end, String region) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected CarType getMostPopularCarTypeIn(Object ms, String carRentalCompanyName, int year) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object getNewReservationSession(String name) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Object getNewManagerSession(String name, String carRentalName) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void checkForAvailableCarTypes(Object session, Date start, Date end) throws Exception {
+		//TODO: incorporate session.
 		try {
 			Set<CarType> carTypes = this.crc.getAvailableCarTypes(start, end);
 			carTypes.forEach(carType -> System.out.println(carType.toString()));
 		} catch(Exception e) {
 			throw new UnsupportedOperationException();
 		}
-	}
-
-	/**
-	 * Retrieve a quote for a given car type (tentative reservation).
-	 * 
-	 * @param	clientName 
-	 * 			name of the client 
-	 * @param 	start 
-	 * 			start time for the quote
-	 * @param 	end 
-	 * 			end time for the quote
-	 * @param 	carType 
-	 * 			type of car to be reserved
-	 * @param 	region
-	 * 			region in which car must be available
-	 * @return	the newly created quote
-	 *  
-	 * @throws 	Exception
-	 * 			if things go wrong, throw exception
-	 */
-	@Override
-	protected Quote createQuote(String clientName, Date start, Date end, 
-			String carType, String region) throws Exception {
-		ReservationConstraints constraints = new ReservationConstraints(start, end, carType, region);
-		try {
-			Quote quote = this.crc.createQuote(constraints, clientName);
-			System.out.println(quote.toString());
-			return quote;
-		} catch(Exception e) {
-			throw new UnsupportedOperationException(e.toString());
-		}
 		
 	}
 
-	/**
-	 * Confirm the given quote to receive a final reservation of a car.
-	 * 
-	 * @param 	quote 
-	 * 			the quote to be confirmed
-	 * @return	the final reservation of a car
-	 * 
-	 * @throws 	Exception
-	 * 			if things go wrong, throw exception
-	 */
 	@Override
-	protected Reservation confirmQuote(Quote quote) throws Exception {
-		try {
-			Reservation reservation = this.crc.confirmQuote(quote);
-			System.out.println(reservation.toString());
-			return reservation;
-		} catch (Exception e) {
-			throw new UnsupportedOperationException();
-		}
-	}
-	
-	/**
-	 * Get all reservations made by the given client.
-	 *
-	 * @param 	clientName
-	 * 			name of the client
-	 * @return	the list of reservations of the given client
-	 * 
-	 * @throws 	Exception
-	 * 			if things go wrong, throw exception
-	 */
-	@Override
-	protected List<Reservation> getReservationsByRenter(String clientName) throws Exception {
-		try {
-			List<Reservation> list = this.crc.getRenterReservations(clientName);
-			for (Reservation res : list) {
-				System.out.println(res);
+	protected void addQuoteToSession(Object session, String name, Date start, Date end, String carType, String region)
+			throws Exception {
+			// TODO incorporate session
+			ReservationConstraints constraints = new ReservationConstraints(start, end, carType, region);
+			try {
+				Quote quote = this.crc.createQuote(constraints, clientName);
+				System.out.println(quote.toString());
+				return quote;
+			} catch(Exception e) {
+				throw new UnsupportedOperationException(e.toString());
 			}
-			return list;
-		} catch (Exception e) {
-			throw new UnsupportedOperationException();
-		}
+		
 	}
 
-	/**
-	 * Get the number of reservations for a particular car type.
-	 * 
-	 * @param 	carType 
-	 * 			name of the car type
-	 * @return 	number of reservations for the given car type
-	 * 
-	 * @throws 	Exception
-	 * 			if things go wrong, throw exception
-	 */
 	@Override
-	protected int getNumberOfReservationsForCarType(String carType) throws Exception {
+	protected List confirmQuotes(Object session, String name) throws Exception {
+		return null;
+	}
+
+	@Override
+	protected int getNumberOfReservationsForCarType(Object ms, String carRentalName, String carType) throws Exception {
+		// TODO Auto-generated method stub
 		try {
 			int number = this.crc.getNumberOfReservationsForCarType(carType);
 			return number;
 		} catch (Exception e) {
 			throw new UnsupportedOperationException();
-		}
+		}		
 	}
 }
